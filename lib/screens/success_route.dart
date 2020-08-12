c0c4c5d94021248c2caab1e0d1c5403ab6a93292
James Smith <theworld969@hotmail.com>
2020-08-11 20:53:28 -0600

    Add alternate completion button to success screen

--- lib/screens/success_route.dart
+++ lib/screens/success_route.dart
@@ -10,23 +10,56 @@ class SuccessRoute extends StatelessWidget {
       appBar: AppBar(
         title: Text("Second Success"),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(height: 100),
-          Text(
-            'When you have succeeded, \nclick success.',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                fontSize: 30, fontWeight: FontWeight.bold, color: Colors.blue),
-          ),
-          SizedBox(height: 50),
-          Text(goal,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(height: 100),
+            Text(
+              'When you have succeeded, \nclick success.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue),
+            ),
+            SizedBox(height: 50),
+            Text(
+              goal,
               textAlign: TextAlign.center,
               style: TextStyle(
                 fontSize: 20,
-              )),
-        ],
+              ),
+            ),
+            SizedBox(height: 30),
+            // todo review alternate completion button
+            FlatButton(
+              child: Container(
+                height: 65,
+                width: 200,
+                padding: EdgeInsets.all(15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(Icons.adjust),
+                    SizedBox(width: 10),
+                    Text(
+                      'Success',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ],
+                ),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+                // todo: update goal status to complete and increment completion counter
+              },
+              color: Colors.blue,
+              textColor: Colors.white,
+            ),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
